fa93a40b4a4ecbbcbaf94e65b51d7c936e540cc5
Abanoub Saad <88698601+abanoubsaad7@users.noreply.github.com>
2022-03-03 18:40:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function l2 ()
+{
+  const img1 = document.createElement("img");
+  img1.src ="image/l2.jpg";
+  document.getElementById("mem1").appendChild(img1);
+  document.getElementById("r7la").style.display ="block";
+}
+function ah ()
+{
+  const img2 = document.createElement("img");
+  img2.src ="image/ah.jpg";
+  document.getElementById("mem1").appendChild(img2);
+  document.getElementById("r7la").style.display ="block";
+}
+
+function ah2 ()
+{
+  const img3 = document.createElement("img");
+  img3.src ="image/ah1.jpg";
+  img3.id= "aa";
+  document.getElementById("mem2").appendChild(img3);
+  document.getElementById("r7la").style.display ="block";
+}
+
